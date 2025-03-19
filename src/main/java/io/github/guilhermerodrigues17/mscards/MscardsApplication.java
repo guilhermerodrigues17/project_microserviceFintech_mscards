ad79edee0fb22cf44610adf19188d248fea653cf
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-03-19 17:58:50 -0300

    Initial commit

---
+++ src/main/java/io/github/guilhermerodrigues17/mscards/MscardsApplication.java
@@ -0,0 +1,15 @@
+package io.github.guilhermerodrigues17.mscards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MscardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscardsApplication.class, args);
+	}
+
+}
