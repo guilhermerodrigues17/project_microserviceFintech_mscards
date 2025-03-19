422a9a62af1b21fa63b708e0af25c052a41a177e
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-03-19 18:56:03 -0300

    feat: Implementado endpoint para pesquisa de cards baseado na renda, no CardsResource

--- src/main/java/io/github/guilhermerodrigues17/mscards/application/CardsResource.java
+++ src/main/java/io/github/guilhermerodrigues17/mscards/application/CardsResource.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("cards")
 @RequiredArgsConstructor
@@ -24,5 +26,11 @@ public class CardsResource {
         Card card = representation.toModel();
         service.save(card);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping(params = "income")
+    public ResponseEntity<List<Card>> getCardsByIncome(@RequestParam Long income) {
+        List<Card> cardsList = service.getCardByIncomeLessThanEqual(income);
+        return ResponseEntity.ok(cardsList);
     }
 }
