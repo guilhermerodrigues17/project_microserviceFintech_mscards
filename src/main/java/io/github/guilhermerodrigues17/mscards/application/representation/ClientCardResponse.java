9622c78f2c4a0617b09305b8fa5e773a7408a910
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-03-20 17:24:53 -0300

    feat: Implementado representation para responses de ClientCard

---
+++ src/main/java/io/github/guilhermerodrigues17/mscards/application/representation/ClientCardResponse.java
@@ -0,0 +1,20 @@
+package io.github.guilhermerodrigues17.mscards.application.representation;
+
+import io.github.guilhermerodrigues17.mscards.domain.ClientCard;
+
+import java.math.BigDecimal;
+
+public record ClientCardResponse(
+        String name,
+        String brand,
+        BigDecimal creditLimit
+) {
+
+    public static ClientCardResponse fromModel(ClientCard model) {
+        return new ClientCardResponse(
+                model.getCard().getName(),
+                model.getCard().getBrand().toString(),
+                model.getCreditLimit()
+        );
+    }
+}
